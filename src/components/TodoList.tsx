b2d6ea6301bd5eb19493fb8748d37b1e5f689977
minimi61 <new1573@naver.com>
2022-11-09 00:16:08 +0900

    [feature] toggle done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,23 +14,20 @@ interface  Content  {
 const TodoList = ({ data ,setTodo, todo}:{data: Content, setTodo:  React.Dispatch<React.SetStateAction<Content[]>>, todo: Content[]}) => {
 
   const isDone = () => {
-    const toggle = todo.map(x =>  (x.id === data.id) ? (!x.complete) : x.complete )
-    console.log([...todo],toggle)
-    // setTodo()
-
-    // [...todo].map((x, i) => 
-    //   {if(x.id === data.id){x.complete: !x.complete}}
-    // )
+    const toggle = todo.map((x) => x.id === data.id? {...x, complete: !x.complete} : x)
+    setTodo(toggle)
   }
-  const del = () => {
+
+  const remove = () => {
     setTodo(todo.filter((x) => x.id !== data.id))
   } 
+
   return (
     <div className='Container'>
-      {data.complete ? <div></div> : <div>{data.content}</div>}
+      {data.complete ? <div className='toggleLine'>{data.content}</div> : <div>{data.content}</div>}
       <div>
         <button className='noStyleBtn' onClick={isDone}>✅</button>
-        <button className='noStyleBtn' onClick={del}>❌</button>
+        <button className='noStyleBtn' onClick={remove}>❌</button>
       </div>
     </div>
   )
